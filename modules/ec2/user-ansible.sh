44ba6bfb73f5b1669db56a2cfe50acc95dc7fe32
Suresh <68796452+suppada@users.noreply.github.com>
2024-09-23 21:48:06 -0400

    Update user-ansible.sh

--- modules/ec2/user-ansible.sh
+++ modules/ec2/user-ansible.sh
@@ -1,25 +1,8 @@
 #!/bin/sh -xv
 
-yum update -y
-# yum install java -y
-# yum install java-11-openjdk-devel -y
-# yum install git wget zip unzip nano -y
-# yum install docker -y
-yum install maven jq ansible make -y
-# mkdir /var/jenkins/
-# chmod 777 -R /var/jenkins/
+yum update -y 
+yum install jq ansible -y
 
-#sudo update-alternatives --config java
-
-#gradle config
-# wget https://services.gradle.org/distributions/gradle-5.1.1-bin.zip -P /tmp
-# unzip -d /opt/gradle /tmp/gradle-5.1.1-bin.zip
-#ls /opt/gradle/gradle-5.1.1
-# touch /etc/profile.d/gradle.sh
-# echo 'export GRADLE_HOME=/opt/gradle/gradle-5.1.1' >> /etc/profile.d/gradle.sh
-# echo 'export PATH=${GRADLE_HOME}/bin:${PATH}' >> /etc/profile.d/gradle.sh
-# chmod +x /etc/profile.d/gradle.sh
-# source /etc/profile.d/gradle.sh
 
 #ssm agent install
 yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
@@ -27,9 +10,13 @@ systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
 systemctl status amazon-ssm-agent
 
-#Terraform installation
-# curl -O https://releases.hashicorp.com/terraform/1.1.7/terraform_1.1.7_linux_amd64.zip
-# unzip terraform_1.1.7_linux_amd64.zip -d /usr/local/bin/
+#awscli install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+$PATH='/usr/local/bin'
+
+
 
 #Script to add a user to Linux system
 username='ansible'
@@ -45,6 +32,11 @@ if [ $(id -u) -eq 0 ]; then
                 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
                 useradd -m -p $pass $username
                 [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+                # ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+                # Run me with superuser privileges
+                echo '$username       ALL=(ALL)       ALL' >> /etc/sudoers
+                sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+                service sshd restart
         fi
 else
         echo "Only root may add a user to the system"
