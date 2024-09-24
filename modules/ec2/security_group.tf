485c099b3a085a02fd32b9f9973832fb427e2b87
Suresh <68796452+suppada@users.noreply.github.com>
2024-09-23 21:58:37 -0400

    Update security_group.tf

--- modules/ec2/security_group.tf
+++ modules/ec2/security_group.tf
@@ -12,30 +12,6 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  #HTTP
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-  #Custom TCP
-#   ingress {
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-  #Custom TCP
-#   ingress {
-#     from_port   = 8081
-#     to_port     = 8081
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
   egress {
     from_port   = 0
     to_port     = 0
